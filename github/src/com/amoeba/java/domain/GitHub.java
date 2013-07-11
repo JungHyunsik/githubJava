34e335f59937c5e0e8768e2dc0cf559a0466d715
hyunsikki <hyunsikki@nate.com>
2013-07-11 16:20:32 +0900

    Default Java Project

---
+++ github/src/com/amoeba/java/domain/GitHub.java
@@ -0,0 +1,36 @@
+package com.amoeba.java.domain;
+
+public class GitHub {
+
+	private String id;
+	
+	private String passwd;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the passwd
+	 */
+	public String getPasswd() {
+		return passwd;
+	}
+
+	/**
+	 * @param passwd the passwd to set
+	 */
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+}
